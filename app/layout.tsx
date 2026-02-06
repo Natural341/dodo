ea849a2e2e8af902a2186d93557313e380e7564b
lumosesko-web <lumosesko@gmail.com>
2026-02-06 22:57:47 +0300

    Add default dark mode and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="tr" suppressHydrationWarning>
+    <html lang="tr" className="dark" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
